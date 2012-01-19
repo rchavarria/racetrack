086bf266613575f459dd16fd679501f2316ea097
Ruben Chavarria <txingo@gmail.com>
2012-01-19 09:49:26 +0100

    added validations to Race fields

--- grails-app/domain/racetrack/Race.groovy
+++ grails-app/domain/racetrack/Race.groovy
@@ -10,13 +10,13 @@ class Race {
     Integer maxRunners = 100000
     
     static constraints = {
-        name()
+        name(blank:false, maxSize:50)
         startDate()
         city()
-        state()
-        distance()
-        cost()
-        maxRunners()
+        state(inList:["GA", "NC", "SC", "VA"])
+        distance(min:0.0)
+        cost(min:0.0, max:100.0)
+        maxRunners(min:0, max:100000)
     }
 
     static mapping = {

f1e51328ed0d12a69a1a8664dec2628e5639fc32
Ruben Chavarria <txingo@gmail.com>
2012-01-20 09:18:54 +0100

    added a custom validator for startDate field of Race domain class. added also a custom error message for this validation

--- grails-app/domain/racetrack/Race.groovy
+++ grails-app/domain/racetrack/Race.groovy
@@ -10,16 +10,12 @@ class Race {
     Integer maxRunners = 100000
     
     static constraints = {
-        name(blank:false, maxSize:5)
-        startDate()
+        name(blank:false, maxSize:50)
+        startDate(validator: {return (it > new Date())})
         city()
         state(inList:["GA", "NC", "SC", "VA"])
         distance(min:0.0)
         cost(min:0.0, max:100.0)
         maxRunners(min:0, max:100000)
-    }
-
-    static mapping = {
-        sort "startDate"
     }
 }

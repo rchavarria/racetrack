12f1c36c41cfe0f8b78e86d7ab6ba11a736f6160
Ruben Chavarria <txingo@gmail.com>
2012-01-23 17:48:01 +0100

    added some dummy data to be loaded when application starts

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,65 @@
+import grails.util.Environment
+
+import racetrack.Race
+import racetrack.Registration
+import racetrack.Runner
+
+
 class BootStrap {
 
     def init = { servletContext ->
+        
+        def env = Environment.current.name
+        println "The current environment is ${env}"
+        
+        switch(env){
+            case "development":
+            
+                def jane = new Runner(
+                    firstName:"Jane",
+                    lastName:"Doe",
+                    dateOfBirth:(new Date() - 365*30),
+                    gender:"F",
+                    address:"123 Main St",
+                    city:"Goose",
+                    state:"NC",
+                    zipcode:"12345",
+                    email:"jane@whereever.com"
+                )
+                jane.save()
+                if(jane.hasErrors()){
+                    println jane.errors
+                }
+                
+                def trot = new Race(
+                        name:"Turkey Trot",
+                        startDate:(new Date() + 90),
+                        city:"Duck",
+                        state:"NC",
+                        distance:5.0,
+                        cost:20.0,
+                        maxRunners:350
+                        )
+                trot.save()
+                if(trot.hasErrors()){
+                    println trot.errors
+                }
+                
+                def reg = new Registration(
+                        paid:false,
+                        runner:jane,
+                        race:trot
+                        )
+                reg.save()
+                if(reg.hasErrors()){
+                    println reg.errors
+                }
+                break
+                
+            case "production" : break
+        }
     }
+
     def destroy = {
     }
 }

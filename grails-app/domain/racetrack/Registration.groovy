2e47c257868d6efa7345c3971618c99f71f0729d
Ruben Chavarria <txingo@gmail.com>
2012-01-23 17:28:41 +0100

    added a new domain class, Runner. created a M:M relationship between Race and Runner throught a Registration

--- grails-app/domain/racetrack/Registration.groovy
+++ grails-app/domain/racetrack/Registration.groovy
@@ -1,17 +1,16 @@
 package racetrack
 
 class Registration {
-    String name
-    Date dateOfBirth
-    String gender
-    String address
-    String city
-    String state
-    String zipcode
-    String email
-    Date dateCreated //Note: this is a special name
+
+    Boolean paid
+    Date dateCreated
     
-    static belongsTo = [race:Race]
+    static constraints = {
+        race()
+        runner()
+        paid()
+        dateCreated()
+    }
     
-    static constraints = {}
+    static belongsTo = [race:Race, runner:Runner]
 }
---
+++ grails-app/domain/racetrack/Runner.groovy
@@ -0,0 +1,32 @@
+package racetrack
+
+class Runner {
+    
+    String firstName
+    String lastName
+    Date dateOfBirth
+    String gender
+    String address
+    String city
+    String state
+    String zipcode
+    String email
+    
+    static hasMany = [registrations:Registration]
+    
+    static constraints = {
+        firstName(blank:false)
+        lastName(blank:false)
+        dateOfBirth()
+        gender(inList:["M", "F"])
+        address()
+        city()
+        state()
+        zipcode()
+        email(email:true)
+    }
+    
+    String toString(){
+        "${lastName}, ${firstName} (${email})"
+    }
+}

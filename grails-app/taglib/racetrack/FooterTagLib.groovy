5642739e66d9429b5485ff410625d0bf4cf97fe2
Ruben Chavarria <txingo@gmail.com>
2012-02-03 09:18:18 +0100

    new taglib, copyright, that accepts a parameter. see an example of how to use it in _footer.gsp file

--- grails-app/taglib/racetrack/FooterTagLib.groovy
+++ grails-app/taglib/racetrack/FooterTagLib.groovy
@@ -4,4 +4,9 @@ class FooterTagLib {
     def thisYear = {
         out << new Date().format("yyyy")
     }
+
+    def copyright = {attrs, body ->
+        out << "&copy; " + attrs.startYear + " - "
+        out << thisYear() + " " + body()
+    }
 }
--- grails-app/views/layouts/_footer.gsp
+++ grails-app/views/layouts/_footer.gsp
@@ -1,4 +1,4 @@
 <div id="footer">
 	<hr />
-	&copy; <g:thisYear /> Racetrack, Inc.
+	<g:copyright startYear="1999">Racetrack, Inc.</g:copyright>
 </div>

d233d656a9caaa441a2efc674e6d15b76a80fc95
Ruben Chavarria <txingo@gmail.com>
2012-02-10 13:57:37 +0100

    added two more tests, related to authenticate closure within the controller

--- test/unit/racetrack/UserControllerTests.groovy
+++ test/unit/racetrack/UserControllerTests.groovy
@@ -24,4 +24,27 @@ class UserControllerTests {
         assertNotNull "map can't be null", map
         assertEquals "suziq", map.userInstance.login
     }
+    
+    void testSuccessfulAuthenticate(){
+        def jdoe = new User(login:"jdoe",
+                            password:"password")
+        jdoe.save(flush: true)
+        
+        controller.params.login = "jdoe"
+        controller.params.password = "password"
+        controller.authenticate()
+        assertNotNull controller.session.user
+        assertEquals "jdoe", controller.session.user.login
+    }
+    
+    void testWrongAuthenticate(){
+        def jdoe = new User(login:"jdoe",
+                            password:"password")
+        jdoe.save(flush: true)
+        
+        controller.params.login = "jdoe"
+        controller.params.password = "foo"
+        controller.authenticate()
+        assertTrue controller.flash.message.startsWith("Sorry, jdoe")
+    }
 }
